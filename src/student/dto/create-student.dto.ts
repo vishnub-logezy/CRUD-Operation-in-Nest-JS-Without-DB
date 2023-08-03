55054760023bc8f1c04b57dde09555e39a209c65
Vishnu B kumar <vishnub.logezy@gmail.com>
2023-08-03 13:12:43 +0100

    Update :DTO Validation

--- src/student/dto/create-student.dto.ts
+++ src/student/dto/create-student.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateStudentDto {
-    public id: number;
-    public Fullname: string;
-    public Phone: number;
-  }
-  
+  @ApiProperty()
+  @IsNumberString({ no_symbols: true }, { message: 'ID must be a positive integer.' })
+  public id: number;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Fullname cannot be empty.' })
+  public Fullname: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Phone number cannot be empty.' })
+  @IsNumberString({ no_symbols: true }, { message: 'Phone must be a valid number.' })
+  public Phone: number;
+}
--- src/student/dto/update-student.dto.ts
+++ src/student/dto/update-student.dto.ts
@@ -1,8 +1,18 @@
+import { IsNotEmpty, IsOptional, IsNumberString } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateStudentDto } from './create-student.dto';
 
 export class UpdateStudentDto extends PartialType(CreateStudentDto) {
-    public id: number;
-    public Fullname: string;
-    public Phone: number;
+  @IsOptional()
+  @IsNumberString({ no_symbols: true }, { message: 'ID must be a positive integer.' })
+  public id: number;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'Fullname cannot be empty.' })
+  public Fullname: string;
+
+  @IsOptional()
+  @IsNotEmpty({ message: 'Phone number cannot be empty.' })
+  @IsNumberString({ no_symbols: true }, { message: 'Phone must be a valid number.' })
+  public Phone: number;
 }
